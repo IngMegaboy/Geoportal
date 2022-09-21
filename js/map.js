695db4342cd79b0f2a6d9a41a6998b5563d8399d
IngMegaboy <106101963+IngMegaboy@users.noreply.github.com>
2022-09-21 01:33:57 +0000

    new update

--- js/map.js
+++ js/map.js
@@ -9,7 +9,6 @@ var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 
-
 var googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
     maxZoom: 20,
     subdomains:['mt0','mt1','mt2','mt3']
@@ -25,14 +24,11 @@ var googleHybrid = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z=
     subdomains:['mt0','mt1','mt2','mt3']
 });
 
+
 var googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
     maxZoom: 20,
     subdomains:['mt0','mt1','mt2','mt3']
-});
-
-
-// Add marker in the map
-var marker = L.marker([8,-1]).addTo(map);
+}); 
 
 
 // Add various base map to a dictinary
@@ -41,34 +37,143 @@ var baseLayers = {
    "Streets": googleStreets,
    "Terrain" : googleTerrain,
    "Hybrid": googleHybrid,
-   "Satellite" : googleSat
+   "Satellite" : googleSat,
 };
 
 
-// Add GeoJSON data 
-var regionlayer = L.geoJson(region).addTo(map);
-var raillayer = L.geoJson(rail).addTo(map);
-// var places = L.geoJson(places).addTo(map);
 
 
+
+// ****** Style for polygonLayer *******
+var regionStyle = {
+  color : "red",
+  opacity : 0.9,
+  weight : 2,
+  fill : true,
+}
+
+var districtStyle = {
+  color : "cyan",
+  opacity : 0.9,
+  weight : 1.5, 
+  fillColor : "cyan",
+  fill : true,
+}
+
+
+
+// ******** Style for pointLayer ********
+
+// var healthCenterStyle = {
+//   radius: 4,
+//   fillColor: "red",
+//   color : "red",
+// }
+
+// var placesStyle = {
+//   radius:2,
+//   fill : "green",
+//   color : "green",
+//   fillColor : "green",
+// }
+
+
+
+// ******* Style for polyLine ******** 
+var railStyle = {
+  color : "yellow",
+  weight : 3,
+}
+
+var riverStyle = {
+  color : "blue",
+  opacity : 1,
+  fill : true,
+  fillColor : "blue",
+}
+
+
+//***** Add GeoJSON data for Region (PolygonFeature) *******
+var regionLayer = L.geoJson(region,{
+  style:regionStyle,
+  onEachFeature:function (feature,layer) {
+    layer.bindPopup(feature.properties.region)
+  }
+}).addTo(map);
+
+
+var districtLayer = L.geoJson(district, {
+  style:districtStyle,
+  onEachFeature:function (feature,layer) {
+    layer.bindPopup(feature.properties.DISTRICT)
+  }
+}).addTo(map);
+
+
+// Add GeoJson data for polylineFeatures *********
+var railLayer = L.geoJson(rail,{
+  style:railStyle,
+  onEachFeature:function (feature,layer) {
+    layer.bindPopup(feature.properties.NAME)
+  }
+}).addTo(map);
+
+
+var riverLayer = L.geoJson(river, {
+  style:riverStyle,
+  onEachFeature:function (feature,layer) {
+    layer.bindPopup(feature.properties.name)
+  }
+}).addTo(map);
+
+
+
+// ****** Add geoJson data for pointFeatures **********
+
+// Add marker in the map
+// var marker = L.marker([8,-1]).addTo(map);
+
+
+ // var healthCenterLayer = L.geoJson(healthCenter,{
+ //  pointToLayer:function(feature, latlng) {
+ //  return L.circleMarker(latlng,healthCenterStyle); 
+ // }
+ //  }).addTo(map);
+
+
+ // var placesLayer = L.geoJson(places , {
+ //  pointToLayer: function(feature,latlng) {
+ //    return L.circleMarker(latlng,placesStyle);
+ //  }
+ // }).addTo(map);
+
+
+
+
+// Layer control features
 var overlays = {
-    "Marker": marker,
-    // "Roads": roadsLayer
+    // "Marker": marker,
+    // "HealthCenter" : healthCenterLayer,
+    // "Places" : placesLayer,
+    "RailLine" : railLayer,
+    "Region": regionLayer,
+    "District" : districtLayer,
+    "River" : riverLayer,
 };
-  // Add Layer control to map
+
+// Add Layer control to map
 L.control.layers(baseLayers, overlays,{collapsed:true}).addTo(map);
+
 
 
 // Add leflet browser print control to map
 L.control.browserPrint({position:'topleft'}).addTo(map);
 
-
 // Add Scale to map
 L.control.scale({position:"bottomleft"}).addTo(map);
 
-
 // Mouse hover move coordinate
 map.on("mousemove", function(e){
-	$("#coord").html(`Lat:${e.latlng.lat.toFixed(3)}, Long:${e.latlng.lng.toFixed(3)}`)
+  $("#coord").html(`Lat:${e.latlng.lat.toFixed(3)}, Long:${e.latlng.lng.toFixed(3)}`)
 });
 
